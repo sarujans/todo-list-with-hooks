ec605d7b82e650eaa3879f20183104b4f838851c
Saru Subaschandran <1553511+sasuhub@users.noreply.github.com>
2019-02-08 15:16:08 +0100

    added Two buttons

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,20 @@
 import React from "react";
 
-const Todo = function({ todo, index, completeTodo }) {
-  return <div className="todo">{todo.text}</div>;
+const Todo = function({ todo, index, completeTodo, removeTodo }) {
+  return (
+    <>
+      <div
+        className="todo"
+        style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+      >
+        {todo.text}
+      </div>
+      <>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+        <button onClick={() => removeTodo(index)}>x</button>
+      </>
+    </>
+  );
 };
 
 export default Todo;
